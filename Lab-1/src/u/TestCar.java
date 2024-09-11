b2b6ff25d910d5f02a24808156717a97c470d541
jow20007 <john.whiteside@uconn.edu>
2024-09-11 13:49:34 +0000

    add tests

--- Lab-1/src/u/TestCar.java
+++ Lab-1/src/u/TestCar.java
@@ -11,42 +11,90 @@ public class TestCar {
 
 	Hybrid hybridCar;
 
-	@Before
-	public void setUp() throws Exception {
-		hybridCar  = new Hybrid();
-        double miles = 337.0;
-        double gasGal = 10.0;
-        double kWh = 337.0;
-
+	public void configCar(double miles, double gallons, double kWh) {
         // config gas amount & miles
         hybridCar.setMilesfromGas(miles);
-        hybridCar.setGallonsfromGas(gasGal);
+        hybridCar.setGallonsfromGas(gallons);
 
         // config electric amount & miles
         hybridCar.setElectricMiles(miles);
         hybridCar.setTotalkWh(kWh);
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		hybridCar  = new Hybrid();
     }
 
 	@Test
-	public void MPGTest() {
+	public void MPGTest1() {
+		configCar(337.0, 10.0, 0.0);
 		double expected = 33.7;
 		double actual = hybridCar.calcGasMPG();
 		assertTrue(actual == expected);
 	}
 
 	@Test
-	public void MPGeTest() {
-		double expected = 33.7;
-		double actual = hybridCar.calcMPGe();
-		System.out.println(actual);
+	public void MPGTest2() {
+		configCar(300.0, 15.0, 0.0);
+		double expected = 20.0;
+		double actual = hybridCar.calcGasMPG();
 		assertTrue(actual == expected);
 	}
 
 	@Test
-	public void MPGavgTest() {
-		double expected = 33.7;
+	public void MPGTest3() {
+		configCar(100.0, 4.0, 0.0);
+		double expected = 25.0;
+		double actual = hybridCar.calcGasMPG();
+		assertTrue(actual == expected);
+	}
+
+	@Test
+	public void MPGeTest1() {
+		configCar(337.0, 0.0, 33.7);
+		double expected = 337.0;
+		double actual = hybridCar.calcMPGe();
+		assertTrue(actual == expected);
+	}
+
+	@Test
+	public void MPGeTest2() {
+		configCar(300.0, 0.0, 70);
+		double expected = (300.0 / 70.0) * 33.7;
+		double actual = hybridCar.calcMPGe();
+		assertTrue(actual == expected);
+	}
+
+	@Test
+	public void MPGeTest3() {
+		configCar(100.0, 0.0, 67.4);
+		double expected = 50.0;
+		double actual = hybridCar.calcMPGe();
+		assertTrue(actual == expected);
+	}
+
+	@Test
+	public void MPGavgTest1() {
+		configCar(337.0, 10.0, 33.7);
+		double expected = (337.0 + 33.7) / 2;
 		double actual = hybridCar.calcMPGavg();
-		System.out.println(actual);
+		assertTrue(actual == expected);
+	}
+
+	@Test
+	public void MPGavgTest2() {
+		configCar(300.0, 15.0, 70);
+		double expected = ((300.0 / 70.0) * 33.7 + 20.0) / 2;
+		double actual = hybridCar.calcMPGavg();
+		assertTrue(actual == expected);
+	}
+
+	@Test
+	public void MPGavgTest3() {
+		configCar(100.0, 4.0, 67.4);
+		double expected = (50.0 + 25.0) / 2;
+		double actual = hybridCar.calcMPGavg();
 		assertTrue(actual == expected);
 	}
 }
